357fc9c0f827832ba0b82013036f0249b02d0953
tak n <tkn.infomail@gmail.com>
2021-09-16 21:42:11 +0900

    fix avator size

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -17,9 +17,9 @@ const Main = (props: IMainProps) => (
     <div className="max-w-screen-md mx-auto">
       <div className="border-b border-gray-300">
         <div className="pt-16 pb-8">
-          <figure className="w-12 h-12">
+          <figure>
             <Link href="/">
-              <a><img className="rounded-full" src="/nuovotaka-logo.svg" alt="Avator" /></a>
+              <a><img className="rounded-full" width={50} height={50} src="/nuovotaka-logo.svg" alt="Avator" /></a>
             </Link>
           </figure>
           <div className="font-semibold text-3xl text-gray-900">{Config.title}</div>

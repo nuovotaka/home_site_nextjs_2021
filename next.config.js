d17c0c501722a76706ec07b234bf496aa40c321b
tak n <tkn.infomail@gmail.com>
2021-09-18 15:07:21 +0900

    Downgrade next

--- next.config.js
+++ next.config.js
@@ -8,8 +8,10 @@ module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
   basePath: baseUrl,
-  webpack5: true,
   env: {
     baseUrl: baseUrl,
+  },
+  future: {
+    webpack5: true,
   },
 });

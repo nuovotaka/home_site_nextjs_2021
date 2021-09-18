8392c3d3bae2556b10fa1a744bdecd5144bebc81
tak n <tkn.infomail@gmail.com>
2021-09-18 10:16:44 +0900

    change markdown parser

--- src/utils/Markdown.ts
+++ src/utils/Markdown.ts
@@ -1,19 +1,112 @@
 // TODO: @mapbox/rehype-prism does not have typescript definition
 // @ts-ignore
-import rehypePrism from '@mapbox/rehype-prism';
-import html from 'rehype-stringify';
-import gfm from 'remark-gfm';
-import remarkParse from 'remark-parse';
-import remarkRehype from 'remark-rehype';
-import unified from 'unified';
+import highlightjs from 'highlight.js';
+import marked from 'marked';
+import sanitizeHtml from 'sanitize-html';
+
+marked.setOptions({
+  highlight(code, lang) {
+    return highlightjs.highlightAuto(code, [lang]).value;
+  }, // シンタックスハイライトに使用する関数の設定
+  pedantic: false, // trueの場合はmarkdown.plに準拠する gfmを使用する場合はfalseで大丈夫
+  gfm: true, // GitHub Flavored Markdownを使用
+  breaks: true, // falseにすると改行入力は末尾の半角スペース2つになる
+  sanitize: false, // trueにすると特殊文字をエスケープする
+  silent: false, // trueにするとパースに失敗してもExceptionを投げなくなる
+});
 
 export async function markdownToHtml(markdown: string) {
-  const result = await unified()
-    .use(remarkParse)
-    .use(gfm)
-    .use(remarkRehype)
-    .use(rehypePrism)
-    .use(html)
-    .process(markdown);
-  return result.toString().replace(/@@baseUrl@@/g, process.env.baseUrl || '');
+  const result = await marked(markdown);
+  const clean = sanitizeHtml(result, {
+    allowedTags: [
+      'address',
+      'article',
+      'aside',
+      'footer',
+      'header',
+      'h1',
+      'h2',
+      'h3',
+      'h4',
+      'h5',
+      'h6',
+      'hgroup',
+      'main',
+      'nav',
+      'section',
+      'blockquote',
+      'dd',
+      'div',
+      'dl',
+      'dt',
+      'figcaption',
+      'figure',
+      'hr',
+      'li',
+      'main',
+      'ol',
+      'p',
+      'pre',
+      'ul',
+      'a',
+      'abbr',
+      'b',
+      'bdi',
+      'bdo',
+      'br',
+      'cite',
+      'code',
+      'data',
+      'dfn',
+      'em',
+      'i',
+      'kbd',
+      'mark',
+      'q',
+      'rb',
+      'rp',
+      'rt',
+      'rtc',
+      'ruby',
+      's',
+      'samp',
+      'small',
+      'span',
+      'strong',
+      'sub',
+      'sup',
+      'time',
+      'u',
+      'var',
+      'wbr',
+      'caption',
+      'col',
+      'colgroup',
+      'table',
+      'tbody',
+      'td',
+      'tfoot',
+      'th',
+      'thead',
+      'tr',
+      'img',
+    ],
+    disallowedTagsMode: 'discard',
+    allowedAttributes: {
+      a: ['href', 'name', 'target'],
+      // We don't currently allow img itself by default, but this
+      // would make sense if we did. You could add srcset here,
+      // and if you do the URL is checked for safety
+      img: ['src'],
+    },
+    // Lots of these won't come up by default because we don't allow them
+    selfClosing: ['img', 'br', 'hr', 'area', 'base', 'basefont', 'input', 'link', 'meta'],
+    // URL schemes we permit
+    allowedSchemes: ['http', 'https', 'ftp', 'mailto', 'tel'],
+    allowedSchemesByTag: {},
+    allowedSchemesAppliedToAttributes: ['href', 'src', 'cite'],
+    allowProtocolRelative: true,
+    enforceHtmlBoundary: false,
+  });
+  return clean.toString().replace(/@@baseUrl@@/g, process.env.baseUrl || '');
 }

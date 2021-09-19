89fd1a1eb6f39966333a2b8176858f69b09300f8
tak n <tkn.infomail@gmail.com>
2021-09-19 10:49:47 +0900

    Update posts date and modified date

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -37,7 +37,36 @@ const DisplayPost = (props: IPostProps) => (
     )}
   >
     <h1 className="text-center font-bold text-3xl text-gray-900">{props.title}</h1>
-    <div className="text-center text-sm mb-8">{format(new Date(props.date), 'LLLL d, yyyy')}</div>
+    <div className="text-center text-sm mb-1">
+      <svg
+        className="inline mr-2 w-5 h-5"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+          clipRule="evenodd"
+        />
+      </svg>
+      {format(new Date(props.date), 'LLLL d, yyyy')}
+    </div>
+    <div className="text-center text-sm mb-8">
+      <svg
+        className="inline mr-2 w-5 h-5"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
+          clipRule="evenodd"
+        />
+      </svg>
+      {format(new Date(props.modified_date), 'LLLL d, yyyy')}
+    </div>
 
     <Content>
       <div

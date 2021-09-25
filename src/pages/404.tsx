f16a56e2d4d7259e6b7f2a5c95e6d6f744813669
tak n <tkn.infomail@gmail.com>
2021-09-25 16:51:14 +0900

    add 404 and 500 error pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Custom404() {
+  return (
+    <>
+      <h1 className="font-bold">404 - Page Not Found</h1>
+      <div className="w-1/4 h-1/4">
+        <img src="/assets/images/posts/404.svg" alt="404error" />
+      </div>
+    </>
+  );
+}
